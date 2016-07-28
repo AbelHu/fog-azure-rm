ba979f0b91b8906d82791f70c2b7066e110cc941
Yang Xia <yaxia@microsoft.com>
2016-07-28 16:46:08 +0800

    Support Azure storage blob upload/download

--- lib/fog/azurerm/utilities/general.rb
+++ lib/fog/azurerm/utilities/general.rb
@@ -16,3 +16,7 @@ def validate_params(required_params, input_params)
     raise(ArgumentError, "Missing Parameters: #{missing_params.join(', ')} required for this operation")
   end
 end
+
+def random_string(length)
+  (0...length).map { ('a'..'z').to_a[rand(26)] }.join
+end
